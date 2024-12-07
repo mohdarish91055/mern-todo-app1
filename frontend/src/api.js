636573f1fb68edea9679aee88d68c545b564d1de
mohdarish91055 <arishsaifi91055@gmail.com>
2024-12-07 13:00:26 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://mern-todo-app1.onrender.com/api',
+    baseURL: 'http://localhost:8000/api',
     withCredentials : true,
 });
 
